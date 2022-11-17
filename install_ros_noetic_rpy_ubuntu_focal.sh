ee6ac7be1dc65699e716e661bafdb7e5b69223fe
Julien Geneste <eg_julien@Juliens-MacBook-Pro.local>
2022-11-17 18:12:25 +0100

    fix perm 2

--- install_ros_noetic_rpy_ubuntu_focal.sh
+++ install_ros_noetic_rpy_ubuntu_focal.sh
@@ -1,4 +1,3 @@
-sudo su root
 sudo apt-get update && sudo apt-get -y --quiet --no-install-recommends install \
 		bzip2 \
 		ca-certificates \
@@ -39,15 +38,13 @@ sudo apt-get update && sudo apt-get -y --quiet --no-install-recommends install \
 		zip \
 	&& sudo apt-get -y autoremove \
 	&& sudo apt-get clean autoclean \
-	&& rm -rf /var/lib/apt/lists/{apt,dpkg,cache,log} /tmp/* /var/tmp/*
+	&& sudo rm -rf /var/lib/apt/lists/{apt,dpkg,cache,log} /tmp/* /var/tmp/*
 
-cd /usr/src/gtest \
-	&& mkdir build && cd build \
-	&& cmake .. && make -j$(nproc) \
-	&& find . -name \*.a -exec cp {} /usr/lib \; \
-	&& cd .. && rm -rf build
-
-su dev
+sudo cd /usr/src/gtest \
+	&& sudo mkdir build && cd build \
+	&& sudo cmake .. && make -j$(nproc) \
+	&& sudo find . -name \*.a -exec cp {} /usr/lib \; \
+	&& sudo cd .. && sudo rm -rf build
 
 python3 -m pip install --upgrade pip wheel setuptools
 
@@ -55,50 +52,48 @@ python3 -m pip install argparse argcomplete coverage cerberus empy jinja2 kconfi
 		matplotlib==3.0.* numpy nunavut>=1.1.0 packaging pkgconfig pyros-genmsg pyulog \
 		pyyaml requests serial six toml psutil pyulog wheel jsonschema pynacl
 
-sudo su root
+sudo ln -s /usr/bin/ccache /usr/lib/ccache/cc \
+	&& sudo ln -s /usr/bin/ccache /usr/lib/ccache/c++
 
-ln -s /usr/bin/ccache /usr/lib/ccache/cc \
-	&& ln -s /usr/bin/ccache /usr/lib/ccache/c++
+sudo wget -q https://downloads.sourceforge.net/project/astyle/astyle/astyle%203.1/astyle_3.1_linux.tar.gz -O /tmp/astyle.tar.gz \
+	&& sudo cd /tmp && sudo tar zxf astyle.tar.gz && sudo cd astyle/src \
+	&& sudo make -f ../build/gcc/Makefile -j$(nproc) && sudo cp bin/astyle /usr/local/bin \
+	&& sudo rm -rf /tmp/*
 
-wget -q https://downloads.sourceforge.net/project/astyle/astyle/astyle%203.1/astyle_3.1_linux.tar.gz -O /tmp/astyle.tar.gz \
-	&& cd /tmp && tar zxf astyle.tar.gz && cd astyle/src \
-	&& make -f ../build/gcc/Makefile -j$(nproc) && sudo cp bin/astyle /usr/local/bin \
-	&& rm -rf /tmp/*
+sudo wget -q "https://services.gradle.org/distributions/gradle-6.3-rc-4-bin.zip" -O /tmp/gradle-6.3-rc-4-bin.zip \
+	&& sudo mkdir /opt/gradle \
+	&& sudo cd /tmp \
+	&& sudo unzip -d /opt/gradle gradle-6.3-rc-4-bin.zip \
+	&& sudo rm -rf /tmp/*
 
-wget -q "https://services.gradle.org/distributions/gradle-6.3-rc-4-bin.zip" -O /tmp/gradle-6.3-rc-4-bin.zip \
-	&& mkdir /opt/gradle \
-	&& cd /tmp \
-	&& unzip -d /opt/gradle gradle-6.3-rc-4-bin.zip \
-	&& rm -rf /tmp/*
+sudo git clone https://github.com/eProsima/foonathan_memory_vendor.git /tmp/foonathan_memory \
+	&& sudo cd /tmp/foonathan_memory \
+	&& sudo mkdir build && cd build \
+	&& sudo cmake .. \
+	&& sudo cmake --build . --target install -- -j $(nproc) \
+	&& sudo rm -rf /tmp/*
 
-git clone https://github.com/eProsima/foonathan_memory_vendor.git /tmp/foonathan_memory \
-	&& cd /tmp/foonathan_memory \
-	&& mkdir build && cd build \
-	&& cmake .. \
-	&& cmake --build . --target install -- -j $(nproc) \
-	&& rm -rf /tmp/*
+sudo git clone --recursive https://github.com/eProsima/Fast-DDS.git -b v2.0.2 /tmp/FastDDS-2.0.2 \
+	&& sudo cd /tmp/FastDDS-2.0.2 \
+	&& sudo mkdir build && cd build \
+	&& sudo cmake -DTHIRDPARTY=ON -DSECURITY=ON .. \
+	&& sudo cmake --build . --target install -- -j $(nproc) \
+	&& sudo rm -rf /tmp/*
 
-git clone --recursive https://github.com/eProsima/Fast-DDS.git -b v2.0.2 /tmp/FastDDS-2.0.2 \
-	&& cd /tmp/FastDDS-2.0.2 \
-	&& mkdir build && cd build \
-	&& cmake -DTHIRDPARTY=ON -DSECURITY=ON .. \
-	&& cmake --build . --target install -- -j $(nproc) \
-	&& rm -rf /tmp/*
-
-git clone --recursive https://github.com/eProsima/Fast-DDS-Gen.git -b v1.0.4 /tmp/Fast-RTPS-Gen-1.0.4 \
+sudo git clone --recursive https://github.com/eProsima/Fast-DDS-Gen.git -b v1.0.4 /tmp/Fast-RTPS-Gen-1.0.4 \
 	&& cd /tmp/Fast-RTPS-Gen-1.0.4 \
-	&& gradle assemble \
-	&& gradle install \
-	&& rm -rf /tmp/*
+	&& sudo gradle assemble \
+	&& sudo gradle install \
+	&& sudo rm -rf /tmp/*
 
-useradd --shell /bin/bash -u 1001 -c "" -m user && usermod -a -G dialout user
+sudo useradd --shell /bin/bash -u 1001 -c "" -m user && usermod -a -G dialout user
 
-mkdir /tmp/.X11-unix && \
-	chmod 1777 /tmp/.X11-unix && \
-	chown -R root:root /tmp/.X11-unix
+sudo mkdir /tmp/.X11-unix && \
+	sudo chmod 1777 /tmp/.X11-unix && \
+	sudo chown -R root:root /tmp/.X11-unix
 
-wget --quiet http://packages.osrfoundation.org/gazebo.key -O - | apt-key add - \
-	&& sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -sc` main" > /etc/apt/sources.list.d/gazebo-stable.list' \
+sudo wget --quiet http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add - \
+	&& sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -sc` main" > /etc/apt/sources.list.d/gazebo-stable.list' \
 	&& sudo apt-get update \
 	&& DEBIAN_FRONTEND=noninteractive sudo apt-get -y --quiet --no-install-recommends install \
 		ant \
@@ -122,17 +117,16 @@ wget --quiet http://packages.osrfoundation.org/gazebo.key -O - | apt-key add - \
 		ignition-edifice \
 	&& sudo apt-get -y autoremove \
 	&& sudo apt-get clean autoclean \
-	&& rm -rf /var/lib/apt/lists/{apt,dpkg,cache,log} /tmp/* /var/tmp/*
+	&& sudo rm -rf /var/lib/apt/lists/{apt,dpkg,cache,log} /tmp/* /var/tmp/*
 
-cd
 wget https://github.com/JSBSim-Team/jsbsim/releases/download/v1.1.1a/JSBSim-devel_1.1.1-134.focal.amd64.deb
-dpkg -i JSBSim-devel_1.1.1-134.focal.amd64.deb
+sudo dpkg -i JSBSim-devel_1.1.1-134.focal.amd64.deb
 
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
+sudo curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
 
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list' \
-	&& sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list' \
-	&& sh -c 'echo "deb http://packages.ros.org/ros-shadow-fixed/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-shadow.list' \
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list' \
+	&& sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list' \
+	&& sudo sh -c 'echo "deb http://packages.ros.org/ros-shadow-fixed/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-shadow.list' \
 	&& sudo apt-get update \
 	&& sudo apt-get -y --quiet --no-install-recommends install \
 		geographiclib-tools \
@@ -160,23 +154,17 @@ sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /e
 		python3-wstool \
 		xvfb \
 		vim \
-	&& geographiclib-get-geoids egm96-5 \
+	&& sudo geographiclib-get-geoids egm96-5 \
 	&& sudo apt-get -y autoremove \
 	&& sudo apt-get clean autoclean \
-	&& rm -rf /var/lib/apt/lists/{apt,dpkg,cache,log} /tmp/* /var/tmp/*
-
-su dev
+	&& sudo rm -rf /var/lib/apt/lists/{apt,dpkg,cache,log} /tmp/* /var/tmp/*
 
 pip3 install -U \
 		osrf-pycommon
 
-cd
-
 wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh && sudo bash ./install_geographiclib_datasets.sh
 
-rosdep init && rosdep update
-
-cd
+sudo rosdep init && rosdep update
 
 git clone https://github.com/PX4/PX4-Autopilot.git --recursive /home/$USER/PX4-Firmware/
 
