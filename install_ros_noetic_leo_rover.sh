08c647596e885c3fed594aaa12efe3ab5dd3716b
SnAvX <62673208+SnAvX@users.noreply.github.com>
2023-02-27 21:30:47 +0100

    Update install_ros_noetic_leo_rover.sh

--- install_ros_noetic_leo_rover.sh
+++ install_ros_noetic_leo_rover.sh
@@ -1,3 +1,8 @@
 sudo apt-get update && sudo apt install curl \
                 ros-noetic-desktop-full \
+                python3-rosdep \
+                python3-rosinstall \
+                python3-rosinstall-generator \
+                python3-wstool \
+                build-essential \
                 
